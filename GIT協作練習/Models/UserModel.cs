77c6a086e1233836f02d8aef44cb3d72736266d5
周政輝 <leo604leo604@gmail.com>
2025-04-05 22:04:27 +0800

    建立資料夾

---
+++ GIT協作練習/Models/UserModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GIT協作練習.Models
+{
+    public class UserModel
+    {
+        public String UserName { get; set; }
+        public String Account { get; set; }
+        public String Password { get; set; }
+        public String Address { get; set; }
+            
+    }
+}
---
+++ GIT協作練習/Services/RegisterService.cs
@@ -0,0 +1,7 @@
+namespace GIT協作練習.Services
+{
+    public class RegisterService
+    {
+        
+    }
+}
