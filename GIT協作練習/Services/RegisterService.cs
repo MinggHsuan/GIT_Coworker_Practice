036b8c2f49dcd219fce46dfb81a2fe56a75538c4
周政輝 <leo604leo604@gmail.com>
2025-04-05 22:38:51 +0800

    寄送認證信

--- GIT協作練習/Services/RegisterService.cs
+++ GIT協作練習/Services/RegisterService.cs
@@ -1,4 +1,5 @@
 using GIT協作練習.Models;
+using GIT協作練習.Unility;
 
 namespace GIT協作練習.Services
 {
@@ -9,6 +10,7 @@ namespace GIT協作練習.Services
                 UserModel userModel = new UserModel();
                 userModel.Account = model.Account;
                 userModel.Password = model.Password;
+                Mail.SendEmail(userModel.Account,"註冊認證信","請先按下按鈕進行帳戶認證");
                 return true;
             }
     }
